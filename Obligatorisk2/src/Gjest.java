154cc96061f8719ce423558dc4f189e1d620f70f
Benedicte Karlsen <benedictek1@gmail.com>
2016-02-18 22:13:51 +0100

    La inn sjekk for å se om kortet er sperret.

--- Obligatorisk2/src/Gjest.java
+++ Obligatorisk2/src/Gjest.java
@@ -10,8 +10,10 @@ public class Gjest extends Kort {
 	public boolean sjekkPIN(int pin) {
 		// Usikker på om denne metoden skal returnere true HVIS pin skal sjekkes, 
 		// eller true om pin er sjekket og iorden. går utfra det siste.
-		GregorianCalendar currentTime = new GregorianCalendar();
+		if(this.isSperret())
+			return false;
 		
+		GregorianCalendar currentTime = new GregorianCalendar();
 		// Sjekker om utløpstiden på en uke er gått ut. 1 uke = 604800000 millisekunder. 
 		if((currentTime.getTimeInMillis() - this.getDatoOpprettet().getTimeInMillis()) < 604800000 && this.isRiktigPIN(pin))
 			return true;
