72d373ec96ddf416d8f07c132cafdce09a386d74
Benedicte Karlsen <benedictek1@gmail.com>
2016-02-18 10:10:29 +0100

    Partial implementation of Kort constructor

--- Obligatorisk2/src/Kort.java
+++ Obligatorisk2/src/Kort.java
@@ -1,9 +1,16 @@
 
-public class Kort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test");
+public abstract class Kort {
+	private String fornavn;
+	private String etternavn;
+	private int pinkode;
+	private int kortnummer;
+	private boolean sperretKort;
+	
+	Kort(String fornavn, String etternavn, int pinkode) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.pinkode = pinkode;
+		sperretKort = false;
 	}
 
 }
