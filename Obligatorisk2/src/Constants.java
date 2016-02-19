1f724d5c30b7d9ba90e1529432d96c8e408c5351
Benedicte Karlsen <benedictek1@gmail.com>
2016-02-20 00:17:33 +0100

    Implemented Constants

---
+++ Obligatorisk2/src/Constants.java
@@ -0,0 +1,15 @@
+
+public interface Constants {
+	
+	public abstract String setFirstName(String firstName);
+	public abstract String setSurname(String surname);
+	public abstract String setFullName(String fullName);
+	
+	public abstract String getFirstName();
+	public abstract String getSurname();
+	public abstract String getFullName();
+	
+	public abstract double calculateCredit();
+	public abstract double calculateBonus();
+	
+}
