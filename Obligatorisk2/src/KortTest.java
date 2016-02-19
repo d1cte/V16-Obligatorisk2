bfdb760c0bd1aec3e578f70754b2a45f22fc3651
Benedicte Karlsen <benedictek1@gmail.com>
2016-02-19 13:53:01 +0100

    Uppdated KortTest

--- Obligatorisk2/src/KortTest.java
+++ Obligatorisk2/src/KortTest.java
@@ -4,17 +4,17 @@ public class KortTest {
 
 	public static void main(String[] args) {
 
-		ArrayList<Kort> kortArray = new ArrayList<>();
-		Kort k1 = new Ansatt("Ole Olsen", 1234);
-		Kort k2 = new Gjest("Marit Olsen", 9999);
+		ArrayList<Card> kortArray = new ArrayList<>();
+		Card k1 = new Employee("Ole Olsen", 1234);
+		Card k2 = new Guest("Marit Olsen", 9999);
 		
 		kortArray.add(k1);
 		kortArray.add(k2);
 		
-		for (Kort kort : kortArray) {
+		for (Card kort : kortArray) {
 			System.out.println(kort);
-			System.out.println("Kode 1234 er " + (kort.sjekkPIN(1234) ? "gyldig": "ugyldig"));
-			System.out.println("Kode 9999 er " + (kort.sjekkPIN(9999) ? "gyldig": "ugyldig"));
+			System.out.println("Kode 1234 er " + (kort.checkPIN(1234) ? "gyldig": "ugyldig"));
+			System.out.println("Kode 9999 er " + (kort.checkPIN(9999) ? "gyldig": "ugyldig"));
 			System.out.println("--------------------------");
 		}
 
