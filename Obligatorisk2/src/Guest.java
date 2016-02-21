fc2fd5a34e27515177cab38a3bfca9e35d950b29
Benedicte Karlsen <benedictek1@gmail.com>
2016-02-21 23:26:40 +0100

    removed unnecessary code

--- Obligatorisk2/src/Guest.java
+++ Obligatorisk2/src/Guest.java
@@ -1,29 +1,35 @@
-import java.util.GregorianCalendar;
+import java.util.*;
 
 public class Guest extends Card {
 
 	public Guest(String fullName, int pinCode) {
 		super(fullName, pinCode);
 	}
+	
+	
+	public void isExpired() {
+		// Checks if card is expired and blocks the card if needed
+		Calendar currentTime = new GregorianCalendar();
+		// 1 week = 604800000 millisec.
+		if(currentTime.getTimeInMillis() - getDateCreated().getTimeInMillis() > 604800000) 
+			setBlockedCard(true);
+	}
+	
+	
 
 	@Override
 	public boolean checkPIN(int pinCode) {
 		// Usikker på om denne metoden skal returnere true HVIS pin skal sjekkes, 
 		// eller true om pin er sjekket og iorden. går utfra det siste.
-		this.isExpired();
+		isExpired();
 		
-		if(this.isCorrectPIN(pinCode) && !this.isBlocked())
+		if(isCorrectPIN(pinCode) && !isBlocked())
 			return true;
 
 		return false;
 	}
 	
-	void isExpired() {
-		// Checks if card is expired and blocks the card if needed
-		GregorianCalendar currentTime = new GregorianCalendar();
-		// 1 week = 604800000 millisec.
-		if(currentTime.getTimeInMillis() - this.getDateCreated().getTimeInMillis() > 604800000) 
-			this.setBlockedCard(true);
-	}
+
+	
 
 }
