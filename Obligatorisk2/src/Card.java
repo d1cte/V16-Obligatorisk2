a7d9d09871dbadbbe7a574df298b4552d00d2e11
Benedicte Karlsen <benedictek1@gmail.com>
2016-02-22 09:55:30 +0100

    Fixed Clone()

--- Obligatorisk2/src/Card.java
+++ Obligatorisk2/src/Card.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.*;
 
-public abstract class Card implements Cloneable, Comparable<Card>{
+public abstract class Card implements Cloneable, Comparable<Card> {
 	private String fullName;
 	private String firstName;
 	private String surname;
@@ -10,7 +10,7 @@ public abstract class Card implements Cloneable, Comparable<Card>{
 	private boolean blockedCard;
 	private static ArrayList<Integer> allCardNumbers = new ArrayList<>();
 	private Calendar dateCreated;
-	
+
 	Card(String fullName, int pinCode) {
 		setFullName(fullName);
 		this.pinCode = pinCode;
@@ -18,58 +18,59 @@ public abstract class Card implements Cloneable, Comparable<Card>{
 		dateCreated = new GregorianCalendar();
 		setCardNumber();
 	}
-	
+
 	private void setCardNumber() {
-		int cardNumber = (int)(Math.random() * (100000000 - 10000000) + 10000000);
-		
-		while(allCardNumbers.contains(cardNumber)) {
-			cardNumber = (int)(Math.random() * (100000000 - 10000000) + 10000000);
+		int cardNumber = (int) (Math.random() * (100000000 - 10000000) + 10000000);
+
+		while (allCardNumbers.contains(cardNumber)) {
+			cardNumber = (int) (Math.random() * (100000000 - 10000000) + 10000000);
 		}
 		allCardNumbers.add(cardNumber);
 		this.cardNumber = cardNumber;
 	}
-	
+
 	public void setBlockedCard(boolean blockedCard) {
 		this.blockedCard = blockedCard;
 	}
-	
+
 	public void setFullName(String fullName) {
 		String[] allNames = fullName.split(" ");
-		
-		// In case given name (firstName) is made up of several names, like "Tor Even"
+
+		// In case given name (firstName) is made up of several names, like "Tor
+		// Even"
 		String firstName = "";
-		for(int i = 0; i < allNames.length - 1; i++)
+		for (int i = 0; i < allNames.length - 1; i++)
 			firstName += allNames[i] + " ";
-		
+
 		setFirstName(firstName.trim());
 		setSurname(allNames[allNames.length - 1]);
 		this.fullName = this.firstName + " " + surname;
 	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-	
+
 	public boolean isBlocked() {
 		return blockedCard;
 	}
-	
+
 	public boolean isCorrectPIN(int pinCode) {
-		return((pinCode == this.pinCode) ? true : false);
+		return ((pinCode == this.pinCode) ? true : false);
 	}
-	
+
 	public Calendar getDateCreated() {
 		return dateCreated;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public String getSurname() {
 		return surname;
 	}
@@ -77,27 +78,28 @@ public abstract class Card implements Cloneable, Comparable<Card>{
 	public String getFullName() {
 		return fullName;
 	}
-	
+
 	public int getCardNumber() {
 		return cardNumber;
 	}
-	
+
 	public int getPinCode() {
 		return pinCode;
 	}
-	
+
 	@Override
 	public String toString() {
 		String header = String.format("%-20s %-20s %-20s %-20s\n", "Navn", "Kortnummer", "PIN-kode", "Status");
-		String information = String.format("%-20s %-20d %-20d %-20s", fullName, cardNumber, pinCode, ((blockedCard) ? "Sperret": "Aktiv"));
+		String information = String.format("%-20s %-20d %-20d %-20s", fullName, cardNumber, pinCode,
+				((blockedCard) ? "Sperret" : "Aktiv"));
 		return header + information;
 	}
-	
+
 	public abstract boolean checkPIN(int pin);
-	
+
 	@Override
 	public int compareTo(Card c) {
-		if(getSurname().compareTo(c.getSurname()) != 0)
+		if (getSurname().compareTo(c.getSurname()) != 0)
 			return getSurname().compareTo(c.getSurname());
 		else
 			return getFirstName().compareTo(c.getFirstName());
@@ -106,13 +108,12 @@ public abstract class Card implements Cloneable, Comparable<Card>{
 	@Override
 	public Object clone() {
 		try {
-		// Shallow copy
-		Card cardClone = (Card)super.clone();
-		// Deep copy on dateCreated
-		cardClone.dateCreated = (Calendar)(dateCreated.clone());
-		return cardClone;
-		}
-		catch (CloneNotSupportedException ex) {
+			// Shallow copy
+			Card cardClone = (Card) super.clone();
+			// Deep copy on dateCreated
+			cardClone.dateCreated = (Calendar) (dateCreated.clone());
+			return cardClone;
+		} catch (CloneNotSupportedException ex) {
 			return null;
 		}
 	}
